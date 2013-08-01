662348fc8221db83a17ab175a2a84309d7ae5bb4
Nick Sweeting <nikisweeting@gmail.com>
2013-07-31 17:41:48 -0700

    initial commit

---
+++ growler.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# MIT Liscence
+
+
+import os, sys
+
+def growl(content="", title="Growler Guard", icon="./icon.png"):
+	print content, title, icon
+
+growl("how are you?")
